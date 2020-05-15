7eec8f7791a2eaa7af1e30373a106dd84462fd53
Taha Batuhan TÜRK <tbturkk@gmail.com>
2020-05-15 18:09:25 +0300

    LZ77  fonksiyonu bitti dosyaya yazma tamamlandı

--- main.c
+++ main.c
@@ -20,6 +20,8 @@ struct token
     char takipTekrari;
 };
 void dosya_okuma();
+void lz77_dosya_yazma();
+void lz77();
 int dosyanin_uzunlugu=0;
 char *ileri_tampon;
 struct token *veriler;
@@ -29,16 +31,23 @@ void main()
 {
     dosya_okuma();
     LZ77();
-    for(int i=0; i<sayac; i++)
-    {
-        printf("\n(%d,%c)\n",veriler[i].offset_len,veriler[i].takipTekrari);
-    }
+    lz77_dosya_yazma();
+    printf("Orijinal boyut: %d  Encoded boyut: %d",dosyanin_uzunlugu, sayac * sizeof(struct token));
     return 0;
+}
+void lz77_dosya_yazma()
+{
+    FILE * f;
+    if (f = fopen("output.txt", "wb"))
+    {
+        fwrite(veriler, sizeof(struct token), sayac, f);
+        fclose(f);
+    }
+
 }
 
 void LZ77()
 {
-
     int benzerlik_sayisi=0;
     int uzaklik=0;
     int bitti_mi=0;
@@ -46,9 +55,20 @@ void LZ77()
     strcpy(temp_i_tampon,ileri_tampon);
     char *arama_tampon=(char *)malloc((strlen(ileri_tampon)+1)*sizeof(char));
     sprintf(arama_tampon, "");
-    //printf("%s %d",arama_tampon,strlen(arama_tampon));
     while(strlen(temp_i_tampon)>0)
     {
+        if(strlen(temp_i_tampon)==1){//sonda eleman tek kalirsa diye
+            veriler=(struct token*)realloc(veriler,(sayac+1)*sizeof(struct token));
+            veriler[sayac].offset_len= OFFSETLENGTH(0, 0);
+            veriler[sayac].takipTekrari=temp_i_tampon[0];
+            sayac++;
+            for(int k=0; k<strlen(temp_i_tampon); k++)
+            {
+                temp_i_tampon[k]=temp_i_tampon[k+1];
+            }
+            break;
+
+        }
 
         int i=0;//her seferinde temp_i_tamponu ilk karakterden baslatip kontrol etmek icin.
         int varmi=0;//benzerlik buunursa bu deger 1 olur.
@@ -56,18 +76,37 @@ void LZ77()
         uzaklik=0;//karakterin uzakligini tutar.
         int index=strlen(arama_tampon);//i=0 icin arama tamponundaki benzerlik yerini tutar.
         int max_uzaklik=strlen(arama_tampon);
-        if(max_uzaklik>1024){
+        if(max_uzaklik>1024)
+        {
             max_uzaklik=1024;//max uzaklık 2 uzeri 10dan buyuk olamaz
         }
-
+        int max_benzerlik=0;
         for(int j=strlen(arama_tampon); j>=index-max_uzaklik; j--)
         {
 
             if(arama_tampon[j]==temp_i_tampon[i])
             {
+                benzerlik_sayisi=0;
                 uzaklik=strlen(arama_tampon)-j;
                 varmi=1;
-                index=j;
+                int index1=j;
+                benzerlik_sayisi++;//1
+                int x=index1+1,y=1;
+                while(arama_tampon[x]==temp_i_tampon[y])
+                {
+                    if(temp_i_tampon+y==temp_i_tampon+strlen(temp_i_tampon)-1){//benzerlik son eleman ile aynıysa bunu benzerlige katmamak icin
+                        break;
+                    }
+                    benzerlik_sayisi++;
+                    x++;
+                    y++;
+                }
+
+                if(benzerlik_sayisi>=max_benzerlik){
+
+                    uzaklik=strlen(arama_tampon)-j;
+                    index=j;
+                }
             }
         }
         if(varmi==1)
@@ -76,15 +115,8 @@ void LZ77()
             char str[2];
             str[0] = 0;
             str[1] = '\0';
-            benzerlik_sayisi++;//1
-            int x=index+1,y=1;
-            while(arama_tampon[x]==temp_i_tampon[y])
+            if(benzerlik_sayisi+strlen(arama_tampon)==strlen(ileri_tampon)) //son karakterin null olmaması icin
             {
-                benzerlik_sayisi++;
-                x++;
-                y++;
-            }
-            if(benzerlik_sayisi+strlen(arama_tampon)==dosyanin_uzunlugu){//son karakterin null olmaması icin tatli sey mubis
                 bitti_mi=benzerlik_sayisi;
                 bitti_mi--;
             }
@@ -104,15 +136,17 @@ void LZ77()
 
         }
         veriler=(struct token*)realloc(veriler,(sayac+1)*sizeof(struct token));
-        if(bitti_mi!=0){
+        if(bitti_mi!=0)
+        {
             veriler[sayac].offset_len= OFFSETLENGTH(uzaklik, bitti_mi);
             veriler[sayac].takipTekrari=temp_i_tampon[i+bitti_mi];
         }
-        else{
+        else
+        {
             veriler[sayac].offset_len= OFFSETLENGTH(uzaklik, benzerlik_sayisi);
             veriler[sayac].takipTekrari=temp_i_tampon[i+benzerlik_sayisi];
         }
-
+        printf("<%d,%d,%c>\n",uzaklik,benzerlik_sayisi,temp_i_tampon[i+benzerlik_sayisi]);
 
         sayac++;
         for(int k=0; k<=benzerlik_sayisi; k++)//arama_tampona eklenilen karakterleri temp_i_tampondan siler.
@@ -123,7 +157,7 @@ void LZ77()
             }
 
         }
-        printf("%s %d %s\n",arama_tampon,strlen(arama_tampon),temp_i_tampon);
+        //printf("%s %d %s%d\n",arama_tampon,strlen(arama_tampon),temp_i_tampon,uzaklik);
 
         //printf("\n(%d,%c)\n",veriler[sayac-1].offset_len,veriler[sayac-1].takipTekrari);
     }
@@ -133,21 +167,10 @@ void LZ77()
 void dosya_okuma()
 {
     FILE *dosya=fopen("input.txt","r");
-    char line[100];
-
-    if (dosya == NULL)
-    {
-        printf("Dosya acilamadi. Lütfen dosyayi kontrol ediniz!");
-        exit(1);
-    }
-    fgets(line, 100, dosya);
-    ileri_tampon=(char *)malloc(strlen(line)+1);
-    strcpy(ileri_tampon,line);
-    while(fgets(line, 100, dosya)!=NULL)
-    {
-        strcat(ileri_tampon,line);
-    }
-    dosyanin_uzunlugu=strlen(ileri_tampon);
-    //printf("%d %s",strlen(ileri_tampon),ileri_tampon);
+    fseek(dosya, 0, SEEK_END);
+    dosyanin_uzunlugu = ftell(dosya);
+    ileri_tampon=calloc( 1, dosyanin_uzunlugu);
+    fseek(dosya, 0, SEEK_SET);
+    fread(ileri_tampon, 1, dosyanin_uzunlugu, dosya);
     fclose(dosya);
 }
